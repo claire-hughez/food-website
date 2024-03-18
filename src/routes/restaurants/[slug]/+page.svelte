d0e9b4ff4c02225f4f097e2b5864a76704bb89c6
James <jwbarnett@gmail.com>
2024-03-18 21:25:05 +0000

    Add styling to restaurant pages

--- src/routes/restaurants/[slug]/+page.svelte
+++ src/routes/restaurants/[slug]/+page.svelte
@@ -8,14 +8,24 @@
 </script>
 
 <h1 class="mb-4 text-4xl font-extrabold leading-none text-slate">{data.name}</h1>
+
 <div>
-  Category: {category.displayName}
-  <br />
-	{#if data.website}
-    Website: <a href="{data.website}">{data.website}</a>
-  {/if}
-  <br />
-  {#if data.source}
-    How we found it: {data.source}
-  {/if}
+  <table class="text-sm text-left">
+    <tr class="border-b">
+      <th scope="row" class="px-6 py-3 font-medium bg-gray-50">Category</th>
+      <td class="px-6 py-3">{category.displayName}</td>
+    </tr>
+    {#if data.website}
+      <tr class="border-b">
+        <th scope="row" class="px-6 py-3 font-medium bg-gray-50">Website</th>
+        <td class="px-6 py-3"><a href="{data.website}">{data.website}</a></td>
+      </tr>
+    {/if}
+    {#if data.source}
+      <tr >
+        <th scope="row" class="px-6 py-3 font-medium bg-gray-50">How we found it</th>
+        <td class="px-6 py-3">{data.source}</td>
+      </tr>
+    {/if}
+  </table>
 </div>
